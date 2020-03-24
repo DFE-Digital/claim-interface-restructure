8640a41cf5752904cd86a1fbeb6e6c72e067a9c7
Rich Cooley <rich@dxw.com>
2020-03-24 09:08:49 +0000

    Add in tabs on two pages to test

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,20 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.get("/claim/student-loan/student-loan", function(req, res) {
+  res.render("claim/student-loan/student-loan");
+});
+
+router.post("/claim/student-loan/student-loan", function(req, res) {
+  let loanAmount = req.session.data["loanAmount"];
+
+  if (loanAmount === "Yes") {
+    res.redirect("/claim/student-loan/current-school-subject");
+  } else if (loanAmount === "No") {
+    res.redirect("/claim/student-loan/decision");
+  } else {
+    res.redirect("#notes");
+  }
+});
+
 module.exports = router
