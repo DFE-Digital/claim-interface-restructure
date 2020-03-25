4e66be187bf171654bc5ad2eeaafffa2b972fc6b
Rich Cooley <rich@dxw.com>
2020-03-25 14:12:41 +0000

    Stuff to show Tekin

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,19 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+
+router.get("/claim/student-loan/index", function(req, res) {
+  if (req.session.data.supportTicketRaised){
+    res.render("claim/student-loan/index-with-support");
+  } else{
+    res.render("claim/student-loan/index");
+  }
+});
+
+router.post("/claim/student-loan/employment", function(req, res) {
+    res.redirect("/claim/student-loan/student-loan");
+});
+
 router.get("/claim/student-loan/employment", function(req, res) {
   res.render("claim/student-loan/employment");
 });
@@ -17,15 +30,19 @@ router.get("/claim/student-loan/student-loan", function(req, res) {
 });
 
 router.post("/claim/student-loan/student-loan", function(req, res) {
-  let loanAmount = req.session.data["loanAmount"];
+  res.redirect("/claim/student-loan/current-school-subject");
 
-  if (loanAmount === "Yes") {
-    res.redirect("/claim/student-loan/current-school-subject");
-  } else if (loanAmount === "No") {
-    res.redirect("/claim/student-loan/decision");
-  } else {
-    res.redirect("#notes");
-  }
+  // let loanAmount = req.session.data["loanAmount"];
+
+  // if (loanAmount === "Yes") {
+  //   res.redirect("/claim/student-loan/current-school-subject");
+  // } else if (loanAmount === "No") {
+  //   res.redirect("/claim/student-loan/decision");
+  // } else {
+  //   req.session.data.supportTicketRaised = true;
+  //   console.log(req.session.data.supportTicketRaised);
+  //   res.redirect("#notes");
+  // }
 });
 
 module.exports = router
